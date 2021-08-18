20f0d6dd2a153ae00b825fcd4ac13cbf989628b4
Robert Carr <robertlcarr@gmail.com>
2021-08-17 22:47:16 -0400

    Initial commit

--- {{cookiecutter.repo_name}}/{{cookiecutter.func_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.func_name}}.py
@@ -1,6 +1,11 @@
 import os
+import logging
+
 from datetime import datetime
 from urllib.request import urlopen
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 SITE = os.environ['site']  # URL of the site to check, stored in the site environment variable
 EXPECTED = os.environ['expected']  # String expected to be on the page, stored in the expected environment variable
@@ -22,10 +27,37 @@ def lambda_handler(event, context):
         if not validate(urlopen(SITE).read()):
             raise Exception('Validation failed')
     except:
-        print('Check failed!')
+        logger.error('Check failed!')
         raise
     else:
-        print('Check passed!')
+        logger.info('Check passed!')
         return event['time']
     finally:
-        print('Check complete at {}'.format(str(datetime.now())))
+        logger.info('Check complete at {}'.format(str(datetime.now())))
+
+    
+# If being called locally, call the handler
+if __name__ == '__main__':
+    import os
+    import json
+    import sys 
+
+    logging.basicConfig()
+    event = {}
+    context = {}
+
+    # TODO if argv[1], read contents, parse into json
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+        with open(input_file, 'r') as f:
+            data = f.read()
+        event = json.loads(data)
+
+    result = lambda_handler(event, context)
+    output = json.dumps(
+        result,
+        sort_keys=True,
+        indent=4,
+        separators=(',', ':')
+    )
+    logger.info(output)
