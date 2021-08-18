7be9db06f33883309c3e03a57649c102f81c6366
Robert Carr <robertlcarr@gmail.com>
2021-08-17 22:54:42 -0400

    Initial commit

--- {{cookiecutter.repo_name}}/{{cookiecutter.func_name}}.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.func_name}}.py
@@ -1,39 +1,13 @@
-import os
 import logging
 
 from datetime import datetime
-from urllib.request import urlopen
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-SITE = os.environ['site']  # URL of the site to check, stored in the site environment variable
-EXPECTED = os.environ['expected']  # String expected to be on the page, stored in the expected environment variable
-
-
-def validate(res):
-    '''Return False to trigger the canary
-
-    Currently this simply checks whether the EXPECTED string is present.
-    However, you could modify this to perform any number of arbitrary
-    checks on the contents of SITE.
-    '''
-    return EXPECTED in res
-
-
 def lambda_handler(event, context):
-    print('Checking {} at {}...'.format(SITE, event['time']))
-    try:
-        if not validate(urlopen(SITE).read()):
-            raise Exception('Validation failed')
-    except:
-        logger.error('Check failed!')
-        raise
-    else:
-        logger.info('Check passed!')
-        return event['time']
-    finally:
-        logger.info('Check complete at {}'.format(str(datetime.now())))
+    logger.info('Inside handler...')
+    logger.info('Handler complete at {}'.format(str(datetime.now())))
 
     
 # If being called locally, call the handler
@@ -41,7 +15,7 @@ if __name__ == '__main__':
     import os
     import json
     import sys 
-
+ 
     logging.basicConfig()
     event = {}
     context = {}
